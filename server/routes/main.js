bbfa201550b9405a101f59761d94d2d12df3d58a
Your Name <you@example.com>
2023-09-09 09:57:39 +0800

    first commit

---
+++ server/routes/main.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+// Routes
+router.get('', (req, res) => {
+  const locals = {
+    title: 'Nodejs Blog',
+    description: 'Simple Blog created with NodeJs, Express & MongoDb',
+  };
+
+  res.render('index', { locals });
+});
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+router.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+module.exports = router;
